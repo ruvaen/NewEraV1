f2b08e181292c6b3e4910f19761c3f6493a88891
serhanozalp <85905439+serhanozalp@users.noreply.github.com>
2021-06-15 01:04:43 +0300

    SerhanV1

--- Assets/serhan.cs
+++ Assets/serhan.cs
@@ -2,9 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class alper : MonoBehaviour
+public class serhan : MonoBehaviour
 {
-    float x = 0;
     // Start is called before the first frame update
     void Start()
     {
